7410fdd4d6b92a9785046bb5e2ad3693b2c07e6d
alexraduca01 <alex.raduca01@icloud.com>
2024-02-10 15:44:58 +0100

    initial commit

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>home <i class="fa fa-home"></i></h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
