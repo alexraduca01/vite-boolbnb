a161330b412817cf3f4c579ba64994a7d79a1bb9
alexraduca01 <alex.raduca01@icloud.com>
2024-02-12 16:16:53 +0100

    fixed AppHeader responsivness

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,13 +2,13 @@
     <div class="w-100 bg-rich-black p-2">
         <div class="container text-white">
             <div class="row justify-content-between align-items-center">
-                <div class="col-sm-10">
+                <div class="col-10">
                     <div class="input-group flex-nowrap">
                         <span style="transform: translate(27px, 6px); z-index: 1000;" id="addon-wrapping"><i class="fa-solid text-black fa-magnifying-glass"></i></span>
                         <input type="text" @keyup.enter="searchApartments()" v-model="userInput" class="form-control ps-5 rounded-pill" placeholder="Search" aria-label="Username" aria-describedby="addon-wrapping">
                     </div>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-2">
                     <a href="http://localhost:8000">login</a>
                 </div>
             </div>
