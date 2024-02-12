9247063105ade3e12d024b8446281c4571d1c9d5
federicomanni97 <manni_federico@libero.it>
2024-02-12 16:23:20 +0100

    show setup

--- src/pages/AppShow.vue
+++ src/pages/AppShow.vue
@@ -1,7 +1,38 @@
 <template>
-    <div>
-        Show
-    </div>
+    <div class="container">
+        <div>
+          <h1>Nome Appartamento</h1>
+        </div>
+        <div>
+            <h3>img Appartamento</h3>
+        </div>
+        <div>
+            <p>Città</p>
+            <p>Nazione</p>
+        </div>
+        <div>
+            <p>Ospiti</p>
+            <p>Camere da letto</p>
+            <p>Bagni</p>
+            <p>Letti</p>
+        </div>
+        <div>
+            <h4>Nome Host</h4>
+        </div>
+        <div>
+            <h5>Servizi</h5>
+            <p>Servizio 1</p>
+            <p>Servizio 2</p>
+        </div>
+        <div>
+            <h2>Dove ti troverai</h2>
+            <h3>Mappa TomTom</h3>
+        </div>
+        <div>
+            <h3>Nome Host</h3>
+            <span class="btn btn-primary">Contatta l'host</span>
+        </div>
+    </div>  
 </template>
 
 <script>
