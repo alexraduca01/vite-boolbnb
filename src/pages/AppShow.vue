726a7d6ea4485e46425d2971a6223569273861d3
federicomanni97 <manni_federico@libero.it>
2024-02-12 18:19:06 +0100

    show dynamic setups added

--- src/pages/AppShow.vue
+++ src/pages/AppShow.vue
@@ -1,43 +1,63 @@
 <template>
     <div class="container">
-        <div>
-          <h1>Nome Appartamento</h1>
-        </div>
-        <div>
-            <h3>img Appartamento</h3>
-        </div>
-        <div>
-            <p>Città</p>
-            <p>Nazione</p>
-        </div>
-        <div>
-            <p>Ospiti</p>
-            <p>Camere da letto</p>
-            <p>Bagni</p>
-            <p>Letti</p>
-        </div>
-        <div>
-            <h4>Nome Host</h4>
-        </div>
-        <div>
-            <h5>Servizi</h5>
-            <p>Servizio 1</p>
-            <p>Servizio 2</p>
-        </div>
-        <div>
-            <h2>Dove ti troverai</h2>
-            <h3>Mappa TomTom</h3>
-        </div>
-        <div>
-            <h3>Nome Host</h3>
-            <span class="btn btn-primary">Contatta l'host</span>
+        <div class="row">
+            <div class="col-sm-12">
+                <div>
+                    <h3>{{ apartment.title}}</h3>
+                </div>
+                <div>
+                    <img :src="store.imgBasePath + apartment.cover_img" alt="" class="w-100">
+                </div>
+                <div>
+                    <h6>Rooms: {{apartment.rooms}}</h6>
+                    <h6>Bedrooms: {{ apartment.beds }}</h6>
+                    <h6>Bathrooms: {{ apartment.bathrooms }}</h6>
+                </div>
+                <div>
+                    <h6> Host Name: {{ apartment.user.name + ' ' + apartment.user.surname }}</h6>
+                </div>
+                <h6>Services</h6>
+                <!-- <div v-for="service in apartment.service">
+                    <ul>
+                        <li>{{ service }}</li>
+                    </ul>
+                </div> -->
+                <div>
+                    <h4>Dove ti troverai</h4>
+                    <h4>Mappa TomTom</h4>
+                </div>
+                <div>
+                    <h4>Nome Host</h4>
+                    <span class="btn btn-primary">Contatta l'host</span>
+                </div>
+                <div id="map"></div>
+            </div>
         </div>
     </div>  
 </template>
 
 <script>
+import axios from 'axios';
+import { store } from '../store.js';
     export default {
-        name: 'AppShow'
+        name: 'AppShow',
+        data(){
+            return {
+                store,
+                apartment: [],
+            }
+        },
+        methods:{
+            getApartments(){
+                axios.get(`${this.store.apiUrl}apartments/${this.$route.params.slug}`).then((res) => {
+                    this.apartment = res.data
+                    console.log(this.apartment);
+                })
+            },
+        },
+        created(){
+            this.getApartments();
+        }
     }
 </script>
 
