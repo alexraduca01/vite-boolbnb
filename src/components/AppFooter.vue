879f81ad3ff82b89552f6601e072715bc5da3eb5
AugustoM94 <email@github.com>
2024-02-13 17:10:34 +0100

    fix on footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,73 +3,29 @@
             <section class=" container">
                <div class="row">
                 <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-                  <h6 class="text-uppercase">Assistenza</h6>
-
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a class="text-body" href="#!">Centro Assistenza</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">AirCover</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">Antidiscriminazione</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">Supporto alla disabilità</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">Opzioni di cancellazione</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">Segnala problema nel quartiere</a>
-                    </li>
-                  </ul>
+                     <h6 class="text-uppercase">Assistenza</h6>
+                   <ul class="list-unstyled mb-0">
+                      <li v-for="(item, index) in assistenza" :key="index">
+                        <a class="text-body" :href="item.url">{{ item.title }}</a>
+                      </li>
+                    </ul>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
                   <h6 class="text-uppercase">Ospitare</h6>
+                       <ul class="list-unstyled mb-0">
+                          <li v-for="(item, index) in ospitare" :key="index">
+                            <a class="text-body" :href="item.url">{{ item.title }}</a>
+                          </li>
+                        </ul>
 
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a class="text-body" href="#!">Apri un BoolBnb</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">AirCover per gli host</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">Risorse per gli host</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">Forum della community</a>
-                    </li>
-                    <li>
-                        <a class="text-body" href="#!">Ospitare responsabilmente</a>
-                    </li>
-                  </ul>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
                   <h6 class="text-uppercase">BoolBnb</h6>
-
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a class="text-body" href="#!">Newsroom</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">Nuove funzionalità</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">Opportunità di lavoro</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">Investitori</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">Gift Card</a>
-                    </li>
-                    <li>
-                      <a class="text-body" href="#!">Soggiorni con BoolBnb.org</a>
-                    </li>
-                  </ul>
+                      <ul class="list-unstyled mb-0">
+                            <li v-for="(item, index) in Boolbnb" :key="index">
+                              <a class="text-body" :href="item.url">{{ item.title }}</a>
+                            </li>
+                      </ul>
                 </div>
               </div>
             </section>               
@@ -118,7 +74,34 @@
 
 <script>
     export default {
-        name: 'AppFooter'
+        name: 'AppFooter',
+        data() {
+            return {
+              assistenza:[
+                        { title: 'Centro Assistenza', url: '#!' },
+                        { title: 'AirCover', url: '#!' },
+                        { title: 'Antidiscriminazione', url: '#!' },
+                        { title: 'Supporto alla disabilità', url: '#!' },
+                        { title: 'Opzioni di cancellazione', url: '#!' },
+                        { title: 'Segnala problema nel quartiere', url: '#!' },
+                ],
+              ospitare:[
+                        { title: 'Apri un BoolBnb', url: '#!' },
+                        { title: 'AirCover per gli host', url: '#!' },
+                        { title: 'Risorse per gli host', url: '#!' },
+                        { title: 'Forum della community', url: '#!' },
+                        { title: 'Ospitare responsabilmente', url: '#!' },
+              ],
+              Boolbnb:[
+                        { title: 'Newsroom', url: '#!' },
+                        { title: 'Nuove funzionalità', url: '#!' },
+                        { title: 'Opportunità di lavoro', url: '#!' },
+                        { title: 'Investitori', url: '#!' },
+                        { title: 'Gift Card', url: '#!' },
+                        { title: 'Soggiorni con BoolBnb.org', url: '#!' },
+                ],
+            }
+        }
     }
 </script>
 
