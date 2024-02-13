7e28baa0ffc4d572b59bfaae61c736bf02baa913
AugustoM94 <email@github.com>
2024-02-13 12:02:40 +0100

    added footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,80 @@
 <template>
-    <div>
+    <footer class="bg-body-tertiary  ">
+            <section class="py-5 container">
+               <div class="row">
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                  <h6 class="text-uppercase">Assistenza</h6>
 
-    </div>
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a class="text-body" href="#!">Centro Assistenza</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">AirCover</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">Antidiscriminazione</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">Supporto alla disabilità</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">Opzioni di cancellazione</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">Segnala problema nel quartiere</a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                  <h6 class="text-uppercase">Ospitare</h6>
+
+                  <ul class="list-unstyled mb-0 text-whit">
+                    <li>
+                      <a class="text-body" href="#!">Apri un BoolBnb</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">AirCover per gli host</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">Risorse per gli host</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">Forum della community</a>
+                    </li>
+                    <li>
+                        <a class="text-body" href="#!">Ospitare responsabilmente</a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                  <h6 class="text-uppercase">BoolBnb</h6>
+
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a class="text-body" href="#!">Newsroom</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">Nuove funzionalità</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">Opportunità di lavoro</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">Investitori</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">Gift Card</a>
+                    </li>
+                    <li>
+                      <a class="text-body" href="#!">Soggiorni con BoolBnb.org</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </section>               
+
+        </footer>
 </template>
 
 <script>
@@ -11,5 +84,13 @@
 </script>
 
 <style lang="scss" scoped>
-
+a{
+text-decoration: none;
+}
+li{
+    padding: 10px;
+}
+h6{
+    padding: 10px;
+}
 </style>
