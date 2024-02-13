35448ddf3e3428c5dabaee42a82c83b018e8ee98
federicomanni97 <manni_federico@libero.it>
2024-02-13 14:59:57 +0100

    show fixed

--- src/pages/AppShow.vue
+++ src/pages/AppShow.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="container">
+    <div>
+        <img :src="store.imgBasePath + apartment.cover_img" alt="" class="w-100">
+    </div>
+    <div class="container bg-prussian-blue">
         <div class="row">
-            <div class="col-sm-12">
-                <div>
-                    <h3>{{ apartment.title}}</h3>
+            <div class="col-sm-12 text-white">
+                <div class="pt-3">
+                    <h2 class="fst-italic">{{ apartment.title}}</h2>
                 </div>
-                <div>
-                    <img :src="store.imgBasePath + apartment.cover_img" alt="" class="w-100">
+                <div class="py-2">
+                    <h6 class="fst-italic"> Host Name: <span class="text-danger">{{ apartment.user.name + ' ' + apartment.user.surname }}</span></h6>
                 </div>
+                <h5 class="py-2">Cosa troverai:</h5>
                 <div>
-                    <h6>Rooms: {{apartment.rooms}}</h6>
-                    <h6>Bedrooms: {{ apartment.beds }}</h6>
-                    <h6>Bathrooms: {{ apartment.bathrooms }}</h6>
-                </div>
-                <div>
-                    <h6> Host Name: {{ apartment.user.name + ' ' + apartment.user.surname }}</h6>
+                    <p><i class="fa-solid fa-building"></i> Rooms: {{apartment.rooms}}</p>
+                    <p><i class="fa-solid fa-bed"></i> Bedrooms: {{ apartment.beds }}</p>
+                    <p><i class="fa-solid fa-bath"></i> Bathrooms: {{ apartment.bathrooms }}</p>
                 </div>
                 <h6>Services</h6>
                 <!-- <div v-for="service in apartment.service">
@@ -28,7 +29,7 @@
                 </div>
                 <div>
                     <h4>Nome Host</h4>
-                    <span class="btn btn-primary">Contatta l'host</span>
+                    <span class="btn btn-primary bg-light text-dark">Contatta l'host</span>
                 </div>
                 <div id="map"></div>
             </div>
