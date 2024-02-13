7848fc4db78340bf601411d3cb6d9ba3661498d3
DavideGila <davide.gila@gmail.com>
2024-02-13 12:13:24 +0100

    added placeholder in the filter the rooms and kilometers section

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,9 @@
             <div class="row justify-content-between align-items-center m-0">
                 <!-- Logo -->
                 <div class="col-2">
-                    <img src="/public/images/logo.png" alt="" style="width: 100px;">
+                    <router-link to="/">
+                        <img src="/public/images/logo.png" alt="" style="width: 100px;">
+                    </router-link>
                 </div>
 
                 <!-- Search Bar  -->
@@ -28,12 +30,46 @@
                     <div class="offcanvas offcanvas-end" :class="showMenu ? 'show' : ''" tabindex="-1"
                         :style="{ visibility: showMenu ? 'visible' : 'hidden' }">
                         <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="">Offcanvas with backdrop</h5>
+                            <h5 class="offcanvas-title" id="">Filtri</h5>
                             <button type="button" class="btn-close text-reset"
                                 @click.prevent="showOffcanvasMenu()"></button>
                         </div>
                         <div class="offcanvas-body">
-                            <p>.....</p>
+                            <form action="">
+                                <!-- Stanze -->
+                                <div class="d-flex gap-5">
+                                    <div>
+                                        <h3 class="fw-bold">Rooms</h3>
+                                        <select name="rooms" id=""></select>
+                                    </div>
+                                    <div>
+                                        <h3 class="fw-bold">Beds</h3>
+                                        <select name="rooms" id=""></select>
+                                    </div>
+                                    <div>
+                                        <h3 class="fw-bold">Bathrooms</h3>
+                                        <select name="rooms" id=""></select>
+                                    </div>
+                                </div>
+
+                                <!-- Chilometri -->
+                                <div id="form-wrapper" class="mt-5">
+                                    <h3 class="fw-bold">Kilometers</h3>
+                                    <div id="kilometers-amount-slider">
+                                        <input type="radio" name="kilometers-amount" id="1" value="1" required>
+                                        <label for="1" data-kilometers-amount="10km"></label>
+                                        <input type="radio" name="kilometers-amount" id="2" value="2" required>
+                                        <label for="2" data-kilometers-amount="20km"></label>
+                                        <input type="radio" name="kilometers-amount" id="3" value="3" required>
+                                        <label for="3" data-kilometers-amount="30km"></label>
+                                        <input type="radio" name="kilometers-amount" id="4" value="4" required>
+                                        <label for="4" data-kilometers-amount="40km"></label>
+                                        <input type="radio" name="kilometers-amount" id="5" value="5" required>
+                                        <label for="5" data-kilometers-amount="50km"></label>
+                                        <div id="kilometers-amount-pos"></div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -76,7 +112,195 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/style/main.scss' as *;
 
-@media screen and (max-width: 575px){
+$number-of-options: 5;
+#form-wrapper {
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+}
+form {
+	width: 90%;
+	max-width: 500px;
+	#kilometers-amount-slider {
+		display: flex;
+		flex-direction: row;
+		align-content: stretch;
+		position: relative;
+		width: 100%;
+		height: 50px;
+		user-select: none;
+		&::before {
+			content: " ";
+			position: absolute;
+			height: 2px;
+			width: 100%;
+			width: calc(100% * (#{$number-of-options - 1} / #{$number-of-options}));
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			background: #000;
+		}
+		input, label {
+			box-sizing: border-box;
+			flex: 1;
+			user-select: none;
+			cursor: pointer;
+		}
+		label {
+			display: inline-block;
+			position: relative;
+			width: 20%;
+			height: 100%;
+			user-select: none;
+			&::before {
+				content: attr(data-kilometers-amount);
+				position: absolute;
+				left: 50%;
+				padding-top: 10px;
+				transform: translate(-50%, 45px);
+				font-size: 14px;
+				letter-spacing: 0.4px;
+				font-weight: 400;
+				white-space: nowrap;
+				opacity: 0.85;
+				transition: all 0.15s ease-in-out;
+			}
+			&::after {
+				content: " ";
+				position: absolute;
+				left: 50%;
+				top: 50%;
+				transform: translate(-50%, -50%);
+				width: 30px;
+				height: 30px;
+				border: 2px solid #000;
+				background: #fff;
+				border-radius: 50%;
+				pointer-events: none;
+				user-select: none;
+				z-index: 1;
+				cursor: pointer;
+				transition: all 0.15s ease-in-out;
+			}
+			&:hover::after {
+				transform: translate(-50%, -50%) scale(1.25);
+			}
+		}
+		input {
+			display: none;
+			&:checked {
+				+ label::before {
+					font-weight: 800;
+					opacity: 1;
+				}
+				+ label::after {
+					border-width: 4px;
+					transform: translate(-50%, -50%) scale(0.75);
+				}
+				~ #kilometers-amount-pos {
+					opacity: 1;
+				}
+				@for $i from 1 through $number-of-options {
+					&:nth-child(#{$i * 2 - 1}) ~ #kilometers-amount-pos {
+						left: #{($i * 20%) - 10%};
+					}
+				}
+			}
+		}
+		#kilometers-amount-pos {
+			display: block;
+			position: absolute;
+			top: 50%;
+			width: 12px;
+			height: 12px;
+			background: #000;
+			border-radius: 50%;
+			transition: all 0.15s ease-in-out;
+			transform: translate(-50%, -50%);
+			border: 2px solid #fff;
+			opacity: 0;
+			z-index: 2;
+		}
+	}
+	&:valid {
+		#kilometers-amount-slider {
+			input {
+				+ label::before {
+					transform: translate(-50%, 45px) scale(0.9);
+					transition: all 0.15s linear;
+				}
+				&:checked + label::before {
+					transform: translate(-50%, 45px) scale(1.1);
+					transition: all 0.15s linear;
+				}
+			}
+		}
+	}
+	& + button {
+		display: block;
+		position: relative;
+		margin: 56px auto 0;
+		padding: 10px 20px;
+		appearance: none;
+		transition: all 0.15s ease-in-out;
+		font-family: inherit;
+		font-size: 24px;
+		font-weight: 600;
+		background: #fff;
+		border: 2px solid #000;
+		border-radius: 8px;
+		outline: 0;
+		user-select: none;
+		cursor: pointer;
+		&:hover {
+			// transform: scale(1.1);
+			background: #000;
+			color: #fff;
+			&:active {
+				transform: scale(0.9);
+			}
+		}
+		&:focus {
+			background: #4caf50;
+			border-color: #4caf50;
+			color: #fff;
+			pointer-events: none;
+			&::before {
+				animation: spin 1s linear infinite;
+			}
+		}
+		&::before {
+			display: inline-block;
+			width: 0;
+			opacity: 0;
+			content: "\f3f4";
+			font-family: "Font Awesome 5 Pro";
+			font-weight: 900;
+			margin-right: 0;
+			transform: rotate(0deg);
+		}
+	}
+	&:invalid + button {
+		pointer-events: none;
+		opacity: 0.25;
+	}
+}
+@keyframes spin {
+	from {
+		transform: rotate(0deg);
+		width: 24px;
+		opacity: 1;
+		margin-right: 12px;
+	}
+	to {
+		transform: rotate(360deg);
+		width: 24px;
+		opacity: 1;
+		margin-right: 12px;
+	}
+}
+
+@media screen and (max-width: 575px) {
     .offcanvas.offcanvas-end {
         width: 100% !important;
     }
