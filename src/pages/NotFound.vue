abdc13298b6b24a01b20a2ef26ae880ff0b0b5ab
AugustoM94 <email@github.com>
2024-02-13 18:24:57 +0100

    added router-link

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -6,8 +6,11 @@
                 <h2 class="mt-5 p-0 mb-5">Oops!</h2>
                 <h3 class="mb-3 ms-2">We can't seem to find the page you're looking for.</h3>
                 <h5 class="mb-3 ms-2 ">Error Code:404</h5>
-                <h5><a class="mb-3 ms-2 text-decoration-none text-black " href="">Ritorna alla homepage</a></h5>
-            </div>
+                <ul>
+                    <li>
+                    <router-link class="text-decoration-none list-unstyled  text-black " to="/" :class="{ active: $route.name === 'home' }">Home</router-link>
+                    </li>
+                </ul>            </div>
             <div class="col-lg-6 col-md-4 mb-3 mt-5">
                 <div class="d-flex justify-content-center">
                     <img src="/public/images/notfound-gif.gif"  alt="logo-notfound">
