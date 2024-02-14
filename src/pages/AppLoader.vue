886bf1f2b73c8d0ea4567d331d149ec4a5b2caa7
AugustoM94 <email@github.com>
2024-02-14 17:07:49 +0100

    added fix loader

--- src/pages/AppLoader.vue
+++ src/pages/AppLoader.vue
@@ -1,15 +1,28 @@
 <template>
     <div>
-
+        <img src="/public/images/home-gif.gif" alt="loader-logo">
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'AppLoader',
-    }
+export default {
+    name: 'AppLoader',
+}
 </script>
 
 <style lang="scss" scoped>
-
+div {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 3000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+img{
+    width: 500px;
+}
 </style>
