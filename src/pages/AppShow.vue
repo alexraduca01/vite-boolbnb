98563e0cc36eec7ed0c26f6ef289c552f87dc781
federicomanni97 <manni_federico@libero.it>
2024-02-14 16:21:27 +0100

    appshow style fixes

--- src/pages/AppShow.vue
+++ src/pages/AppShow.vue
@@ -11,50 +11,44 @@
             </div>
             </div>
         </div>
+            <div class="">
+                <img :src="store.imgBasePath + apartment.cover_img" alt="" class="w-100">
+            </div>
             <div class="container">
             <div class="row">
-            <div class="col-sm-12 p-0 text-white">
-                <div class="">
-                    <img :src="store.imgBasePath + apartment.cover_img" alt="" class="w-100">
-                </div>
+            <div class="col-sm-12  text-white">
                 <div class="pt-3">
-                    <h2 class="">{{ apartment.title}}</h2>
+                    <h2 class="text-uppercase">{{ apartment.title}}</h2>
                 </div>
-                <div class="d-flex w-75 justify-content-between pt-3 align-items-center">
-                    <div>
-                        <img class="bg-black rounded-circle w-25" src="/public/images/logo.png" alt="">
+                <div class="d-flex w-75 py-1">
+                    <div class="fs-3">
+                        <span class="verticalmiddle"> Host: <span class="fw-bold">{{ apartment.user?.name + ' ' + apartment.user?.surname }}</span></span>
                     </div>
-                    <span class="d-flex"> Host Name: <span class="text-danger">{{ apartment.user?.name + ' ' + apartment.user?.surname }}</span></span>
                 </div>
-                <h5 class="pt-4">What you will find:</h5>
-                <div class="d-flex flex-column">
-                    <span class="fs-5"><i class="fa-solid fa-building"></i> Rooms: {{apartment.rooms}}</span>
-                    <span class="fs-5"><i class="fa-solid fa-bed"></i> Bedrooms: {{ apartment.beds }}</span>
-                    <span class="fs-5"><i class="fa-solid fa-bath"></i> Bathrooms: {{ apartment.bathrooms }}</span>
+                <h3 class="pt-4">What you will find:</h3>
+                <div class="d-flex gap-4">
+                    <span class="fs-5 py-1"><i class="fa-solid fa-building"></i> Rooms: {{apartment.rooms}}</span>
+                    <span class="fs-5 py-1"><i class="fa-solid fa-bed fs-6"></i> Bedrooms: {{ apartment.beds }}</span>
+                    <span class="fs-5 py-1"><i class="fa-solid fa-bath"></i> Bathrooms: {{ apartment.bathrooms }}</span>
                 </div>
-                <h5 class="pt-4">Services:</h5>
+                <h3 class="pt-4">Services:</h3>
                 <div >
-                    <ul class="list-unstyled d-flex flex-column align-content-center gap-1">
-                        <li v-for="service in services" class="fs-5"><i :class="service.icon" class=""></i> {{ service.name }}</li>
+                    <ul class="list-unstyled d-flex flex-column align-content-center">
+                        <li v-for="service in services" class="py-1 fs-5 text-capitalize"><i :class="service.icon" class="pe-3"></i> {{ service.name }}</li>
                     </ul>
                 </div>
                 <div>
-                    <h4>Location:</h4>
+                    <h3 class="py-2 text-uppercase">Location:</h3>
                     <div class="row">
                         <div class="col-sm-12 rounded-top">
                             <div id="map" class="rounded"></div>
                         </div>
                     </div>
                 </div>
-                <div class="d-flex justify-content-between py-4">
-                    <div>
-                        <h4 class=""> Host Name: <span class="text-danger">{{ apartment.user?.name + ' ' + apartment.user?.surname }}</span></h4>
-                    </div>
-                </div>
             </div>
             <div>
-            <div>
-                <span class="float-start">
+            <div class="py-4 ">
+                <span class="mb-4 float-start">
                     <button class="btn btn-primary bg-light text-dark" type="button" @click.prevent="showOffcanvasMenu()">
                         Contact Host
                     </button>
@@ -188,6 +182,11 @@ import { store } from '../store.js';
     aspect-ratio: 21 / 9;
     width: 100%;
     max-height: 100%;
+}
+
+.verticalmiddle {
+    vertical-align: middle;
+    
 }
 
 @media screen and (max-width: 575px) {
