80fe2cf64d97ca48cba65cea6dd429d4281385a8
DavideGila <davide.gila@gmail.com>
2024-02-19 17:58:47 +0100

    fixed style

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="position-relative">
-        <AppHeader/>
+        <AppHeader />
         <div class="h-100 bg-prussian-blue home-container">
             <div class="container py-3">
                 <div v-if="store.apartments.length == 0">
                     <h1 class="text-white">No results</h1>
                 </div>
                 <div class="row" v-else>
-                    <router-link v-for="item in store.apartments" @click="postVisuals(item.slug)" class="col-sm-6 col-md-4 col-lg-3 text-white mb-4 text-decoration-none" :to="{name: 'show', params: { slug: item.slug}}">
+                    <router-link v-for="item in store.apartments" @click="postVisuals(item.slug)"
+                        class="col-sm-6 col-md-4 col-lg-3 text-white mb-4 text-decoration-none"
+                        :to="{ name: 'show', params: { slug: item.slug } }">
                         <div v-if="!searchFlag">
                             <div>
                                 <img class="img-fluid my-img" :src="store.imgBasePath + item.cover_img" :alt="item.title">
+                                <!-- <img v-for="(images, index) in imgApartment" :src="store.imgBasePath + images" alt=""> -->
                             </div>
                             <div>
                                 <div class="my-2">
@@ -38,7 +41,7 @@
                 </div>
             </div>
         </div>
-        <AppFooter style="position: absolute; bottom: 0;"/>
+        <AppFooter style="position: absolute; bottom: 0;" />
     </div>
 </template>
 
@@ -47,62 +50,69 @@ import axios from 'axios';
 import { store } from '../store.js'
 import AppHeader from '../components/AppHeader.vue'
 import AppFooter from '../components/AppFooter.vue';
-    export default {
-        components: {
-            AppHeader,
-            AppFooter
-        },
-        data(){
-            return {
-                store,
-                searchFlag: false,
-                userInput: '',
-            }
-        },
-        methods:{
-            getApartments(){
-                axios.get(store.apiUrl + 'apartments').then((res) => {
-                    store.apartments = res.data
-                    // console.log(store.apartments);
-                })
-            },
-            postVisuals(apartmentSlug){
-                axios.post(store.viewsUrl + apartmentSlug + store.viewsEndPoint).then((res) => {
-                    // console.log(res.data);
-                })
-            },
-            
-        },
-        created(){
-            this.getApartments();
+export default {
+    components: {
+        AppHeader,
+        AppFooter
+    },
+    data() {
+        return {
+            store,
+            searchFlag: false,
+            userInput: '',
+            imgApartment: [],
         }
+    },
+    methods: {
+        getApartments() {
+            axios.get(store.apiUrl + 'apartments').then((res) => {
+                store.apartments = res.data
+                // for (let i = 0; i < store.apartments.length; i++) {
+                //     // console.log(store.apartments[i].images);
+                //     for (let a = 0; a < store.apartments[i].images.length; a++) {
+                //         this.imgApartment.push(store.apartments[i].images[a].url);
+                //         // console.log(store.apartments[i].images[a].url);
+                //     }
+                // };
+                // console.log(this.imgApartment);
+            })
+        },
+        postVisuals(apartmentSlug) {
+            axios.post(store.viewsUrl + apartmentSlug + store.viewsEndPoint).then((res) => {
+                // console.log(res.data);
+            })
+        },
+    },
+    created() {
+        this.getApartments();
     }
+}
 </script>
 
 <style lang="scss" scoped>
-
 @use '../assets/style/main.scss' as *;
+
 .home-container {
     padding-bottom: 1200px;
 }
-.my-img{
+
+.my-img {
     aspect-ratio: 1 / 1;
     width: 100%;
     max-height: 100%;
     border-radius: 15px;
 }
 
-@media screen and (min-width: 768px){
-    .home-container{
+@media screen and (min-width: 768px) {
+    .home-container {
         padding-bottom: 920px;
     }
 }
 
-@media screen and (min-width: 992px){
-    .home-container{
+@media screen and (min-width: 992px) {
+    .home-container {
         padding-bottom: 520px;
         min-height: 100vh;
     }
 }
-
 </style>
